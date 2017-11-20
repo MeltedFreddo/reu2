95073d4320c66c9a9029814c321e3ae2b6b80537
Craig Edwards <cdedw1@gmail.com>
2017-11-21 00:41:16 +1100

    Fixed orbital rotation, sorted camera angle and grew and shrank planets based on how "close" to the camera they were

--- Assets/System/SystemViewController.cs
+++ Assets/System/SystemViewController.cs
@@ -21,24 +21,43 @@ public class SystemViewController : MonoBehaviour
         planets = new List<GameObject>();
         for (var i = 0; i < 5; i++)
         {
-            float radius = i / 2f;
+            float radius = i + 2f;
             float angle = i * Mathf.PI * 2 / 5;
-            Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
+            Vector3 pos = new Vector3(Mathf.Cos(angle), 0, 0) * radius;
             planets.Add((GameObject)Instantiate(PlanetPrefab, pos, PlanetPrefab.transform.rotation));
-
+            
             //planets.Add(Instantiate(planet, new Vector3(i + 5, i, 0), Quaternion.identity));
         }
     }
 
     // Update is called once per frame
     void Update()
-    {
-        //TODO orbit planets around star
+    {                
+        //orbit planets around star
         foreach (var planet in planets)
         {
-            planet.transform.RotateAround(theStar.transform.position, new Vector3(0, 0, -1), (float)((Random.value - 0.5) * 30 + 60) * Time.deltaTime); // (1 is left) (-1 is right)
+            var planetPosition = planet.transform.position;
+            var distanceFromSun = (theStar.transform.position - planetPosition).magnitude;            
+            var orbitSpeed = 50 / distanceFromSun;
+            planet.transform.RotateAround(theStar.transform.position, new Vector3(0, 1, 0), orbitSpeed * Time.deltaTime); // (1 is left) (-1 is right)
             planet.transform.rotation = PlanetPrefab.transform.rotation;
-        }
+
+            //update scale
+            var distanceFromZPlane = (new Vector3(planetPosition.x, planetPosition.y, 0) - planetPosition).z;           
+            var prefabScale = PlanetPrefab.transform.localScale.x; //0.1
+            var newScale = 0f;
+            if (distanceFromZPlane != 0)
+            {
+                newScale = prefabScale + ConvertDistanceToScale(5, distanceFromZPlane);
+            }
+            planet.transform.localScale = new Vector3(newScale, newScale, newScale);
+
+        }   
+    }
+
+    private float ConvertDistanceToScale(float maxDistance, float actualDistance)
+    {
+        return (((actualDistance - maxDistance * -1f) * (0.05f - -0.05f)) / (maxDistance - maxDistance * -1f)) + -0.05f;
     }
 
 }
